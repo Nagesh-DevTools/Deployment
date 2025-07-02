03701bec6b5bc993618a54c654df0aceb5d9f748
Nagesh-DevTools <nageshk@devtools.in>
2025-07-02 15:46:11 +0530

    Update test_app.py

--- my-flask-app/tests/test_app.py
+++ my-flask-app/tests/test_app.py
@@ -10,4 +10,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello from Azure Web App" in response.data
+    assert b"Welcome to deployment" in response.data
