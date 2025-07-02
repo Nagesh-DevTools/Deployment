7894927ed5ce6c1082f46324c549024f39019cfc
Nagesh-DevTools <nageshk@devtools.in>
2025-07-02 15:44:15 +0530

    Update app.py

--- my-flask-app/app.py
+++ my-flask-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Azure Web App + GitHub Actions!"
+    return "Welcome to deployment"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8001))
